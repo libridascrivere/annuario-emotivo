8102a00d133fa64d968ece4ed9bff85b4765937f
A Studio B. Project <colorcraftcor@gmail.com>
2026-04-24 23:02:19 +0200

    clear all caches

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-
+self.addEventListener('install', e => { self.skipWaiting(); }); self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k))))); });
